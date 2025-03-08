076f7a50d3b5ba584e5ea49d757634f7eb73f053
Keyvan Sabras <keyvan.sabras@edu.escp.eu>
2025-03-08 18:20:04 +0100

    media upload

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def custom_button(label, path, color: "primary", size: "md", extra_classes: "")
+    link_to label, path, class: "custom-btn btn-#{color} btn-#{size} #{extra_classes}"
+  end
+
+  def custom_form_button(label, color: "primary", size: "md")
+    content_tag :button, label, class: "custom-btn btn-#{color} btn-#{size}", type: "submit", data: { turbo: "false" }
+  end
 end
